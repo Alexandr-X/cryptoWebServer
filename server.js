b7229144792928907681dc951b93c360343bfc27
sasha <ds.alex@tut.by>
2025-05-04 15:45:28 +0300

    some security updataes

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ import { createServer } from "node:http";
 import cors from "cors";
 import { Server } from "socket.io";
 import { createClient } from "@supabase/supabase-js";
+import bcrypt from "bcrypt";
 
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseKey = process.env.SUPABASE_KEY;
-const supabaseUrl = "https://rvtgpibsbmbokrcjydko.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2dGdwaWJzYm1ib2tyY2p5ZGtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MDc0ODYsImV4cCI6MjA2MTA4MzQ4Nn0.2nZ0Ytr2HR2IxIGe8D0w5q9zW0kk0kQQaRKogO-NkBE";
+const saltRounds = 10;
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+
+// const supabaseUrl = "https://rvtgpibsbmbokrcjydko.supabase.co";
+// const supabaseKey =
+//   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2dGdwaWJzYm1ib2tyY2p5ZGtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1MDc0ODYsImV4cCI6MjA2MTA4MzQ4Nn0.2nZ0Ytr2HR2IxIGe8D0w5q9zW0kk0kQQaRKogO-NkBE";
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
@@ -33,14 +36,17 @@ io.on("connection", socket => {
       item => item.email == data.email
     );
     if (arrofSameEmail.length === 0) {
-      await supabase.from("userData").insert([
-        {
-          name: data.name,
-          email: data.email,
-          password: data.password,
-          logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlg1-ZKpUt0a2506DkhjtkH8zHdDtnyUySA&s",
-        },
-      ]);
+      bcrypt.hash(data.password, saltRounds, async function (err, hash) {
+        await supabase.from("userData").insert([
+          {
+            name: data.name,
+            email: data.email,
+            password: hash,
+            logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlg1-ZKpUt0a2506DkhjtkH8zHdDtnyUySA&s",
+          },
+        ]);
+      });
+
       socket.emit("isCorrectReg", true);
     } else socket.emit("isCorrectReg", false);
   });
@@ -52,11 +58,17 @@ io.on("connection", socket => {
       item => item.email == data.email
     );
     if (arrofSameEmail.length === 1) {
-      if (arrofSameEmail[0].password === data.password) {
-        socket.emit("isCorrectReg", true);
-      } else {
-        socket.emit("isCorrectReg", false);
-      }
+      bcrypt.compare(
+        data.password,
+        arrofSameEmail[0].password,
+        function (err, result) {
+          if (result) {
+            socket.emit("isCorrectReg", true);
+          } else if (!result) {
+            socket.emit("isCorrectReg", false);
+          }
+        }
+      );
     } else {
       socket.emit("isCorrectReg", false);
     }
@@ -76,10 +88,10 @@ io.on("connection", socket => {
       });
     } else {
       socket.emit("giveAddInform", {
-        logo: "",
+        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTAlg1-ZKpUt0a2506DkhjtkH8zHdDtnyUySA&s",
         money: 0,
-        arr: "",
-        arrOfPin: "",
+        arr: "[]",
+        arrOfPin: "[]",
       });
     }
   });
